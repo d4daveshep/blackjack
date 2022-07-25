5a9afc53e9b45abadb8a1b93e57d1f98f32a5d86
d4daveshep <david@daveshep.net.nz>
2022-07-25 19:53:14 +1200

    created Strategy class for game strategy and a Move enum for the recommended next move basic unit tests added

--- src/blackjack/game.py
+++ src/blackjack/game.py
@@ -1,9 +1,34 @@
+import enum
+
+
 class Hand():
     def __init__(self, cards: list):
         self.__cards = cards
 
     def __len__(self):
         return len(self.__cards)
+
+    @property
+    def value(self) -> int:
+        total = 0
+        for card in self.__cards:
+            total += card.value
+        return total
+
+class Move(enum.Enum):
+    Hit = "Hit"
+    Sit = "Sit"
+    Bust = "Bust"
+
+class Strategy:
+    @classmethod
+    def decide_move(cls, hand: Hand) -> Move:
+        if hand.value < 17:
+            return Move.Hit
+        elif hand.value <= 21:
+            return Move.Sit
+        else:
+            return Move.Bust
 
 
 
--- src/tests/test_game.py
+++ src/tests/test_game.py
@@ -2,7 +2,7 @@ import sys
 import pytest
 
 sys.path.append("../")
-from blackjack.game import Hand
+from blackjack.game import Hand, Strategy, Move
 from blackjack.pack import Pack
 
 
@@ -13,7 +13,18 @@ def shuffled_pack() -> Pack:
     return pack
 
 
+
 def test_hand_should_draw_two_cards_and_decide_next_move(shuffled_pack):
     hand = Hand(shuffled_pack.deal_cards(2))
 
     assert len(hand) == 2
+    move = Strategy.decide_move(hand)
+    if hand.value < 17:
+        assert move == Move.Hit
+    elif hand.value <= 21:
+        assert move == Move.Sit
+    else:
+        assert move == Move.Bust
+
+
+
