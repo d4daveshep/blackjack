19c412ee30fc84a1d5bd7a337ea32dca3a708df6
d4daveshep <david@daveshep.net.nz>
2022-07-28 20:45:55 +1200

    added method to count_aces in hand modified method to calculate hand value when aces are present unit tests pass

--- src/blackjack/game.py
+++ src/blackjack/game.py
@@ -1,5 +1,7 @@
 import enum
 
+from blackjack.card import CardNames
+
 
 class Hand():
     def __init__(self, cards: list):
@@ -8,11 +10,25 @@ class Hand():
     def __len__(self):
         return len(self.__cards)
 
+    def count_aces(self):
+        aces = 0
+        for card in self.__cards:
+            if card.name == CardNames.Ace:
+                aces += 1
+        return aces
+
     @property
     def value(self) -> int:
         total = 0
         for card in self.__cards:
             total += card.value
+
+        if total > 21 and self.count_aces():
+            for i in range(self.count_aces()):
+                total -= 10
+                if total <= 21:
+                    break
+
         return total
 
 class Move(enum.Enum):
--- src/tests/test_game.py
+++ src/tests/test_game.py
@@ -2,8 +2,55 @@ import sys
 import pytest
 
 sys.path.append("../")
+from blackjack.card import Card, Suits, CardNames
 from blackjack.game import Hand, Strategy, Move
 from blackjack.pack import Pack
+
+
+@pytest.fixture()
+def cards():
+    return {"10": Card(CardNames.Ten, Suits.Spades), "6": Card(CardNames.Six, Suits.Spades),
+            "K": Card(CardNames.King, Suits.Spades), "A": Card(CardNames.Ace, Suits.Spades)}
+
+
+@pytest.fixture()
+def hand_blackjack(cards):
+    return Hand([cards["10"], cards["A"]])
+
+
+@pytest.fixture()
+def hand_K_10(cards):
+    return Hand([cards["K"], cards["10"]])
+
+
+@pytest.fixture()
+def hand_10_6(cards):
+    return Hand([cards["10"], cards["6"]])
+
+
+def test_should_count_aces_in_hand(hand_K_10, hand_blackjack):
+    assert hand_K_10.count_aces() == 0
+    assert hand_blackjack.count_aces() == 1
+
+
+def test_should_add_values_of_cards_in_hand(hand_K_10, hand_blackjack, hand_10_6):
+    assert hand_blackjack.value == 21
+    assert hand_K_10.value == 20
+    assert hand_10_6.value == 16
+
+
+def test_should_add_values_of_cards_in_hand_with_aces(cards):
+    hand = Hand([cards["A"], cards["A"]])  # 2 Aces = 12
+    assert hand.value == 12
+
+    hand = Hand([cards["A"], cards["A"], cards["A"]])  # 3 Aces = 13
+    assert hand.value == 13
+
+    hand = Hand([cards["A"], cards["10"], cards["K"]])
+    assert hand.value == 21
+
+    hand = Hand([cards["A"], cards["10"], cards["K"], cards["A"]])
+    assert hand.value == 22
 
 
 @pytest.fixture()
@@ -11,7 +58,6 @@ def shuffled_pack() -> Pack:
     pack = Pack()
     pack.shuffle()
     return pack
-
 
 
 def test_hand_should_draw_two_cards_and_decide_next_move(shuffled_pack):
@@ -25,6 +71,3 @@ def test_hand_should_draw_two_cards_and_decide_next_move(shuffled_pack):
         assert move == Move.Sit
     else:
         assert move == Move.Bust
-
-
-
