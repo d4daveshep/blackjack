d7f9c184727de8eca4ae14821e808d2294b16e38
d4daveshep <david@daveshep.net.nz>
2022-12-01 16:19:07 +1300

    started on functional test

---
+++ src/tests/functional_test.py
@@ -0,0 +1,43 @@
+
+game = Blackjack(packs=6, min_bet=5.00)
+game.use_stacked_deck() # use a pre-determined order of cards for testing
+
+player1 = Player(name="David", money=100.00)
+game.add_player(player1)
+dealer = game.dealer
+
+# play 5 hands
+game.start()  # waits for all players to place a bet
+
+# hand 1
+player1.place_inital_bet(5.00)  # check balance deducted
+player1.receive_card(dealer.deal_card(), Face.UP)
+dealer.receive_card(dealer.deal_card(), Face.DOWN)
+player1.receive_card(dealer.deal_card(), Face.UP)
+dealer.receive_card(dealer.deal_card(), Face.UP)
+
+if player1.has_blackjack() and dealer.has_blackjack():
+    # stand-off, tie
+    # player receives bet back
+
+elif player1.has_blackjack():
+    # player receives 1.5 x bet
+
+elif dealer.has_blackjack():
+    # player loses bet
+
+else:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
